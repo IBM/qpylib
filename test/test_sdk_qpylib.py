42c7058ea3647396a57fe0b4cbd9311e95d2b981
michael.benson <mbenson@uk.ibm.com>
2019-05-07 13:26:55 +0100

    Fix responses usage

--- test/test_sdk_qpylib.py
+++ test/test_sdk_qpylib.py
@@ -21,11 +21,10 @@ def pre_testing_setup():
 @patch('qpylib.sdk_qpylib.SdkQpylib.get_console_address', return_value = '9.101.234.169')
 @patch('qpylib.sdk_qpylib.SdkQpylib._get_api_auth', return_value = ('testuser', 'testing123'))
 def test_rest_uses_input_values(mock_api_auth, mock_console_address):
-    with responses.RequestsMock() as responses_mock:
-        responses_mock.add('GET', 'https://9.101.234.169/testing_endpoint',
-                           json={'success': True}, status=200)
+    responses.add('GET', 'https://9.101.234.169/testing_endpoint',
+                       json={'success': True}, status=200)
 
-        response = qpylib.REST('GET', 'testing_endpoint', verify='dummycert')
-        assert responses_mock.calls[0].request.url == 'https://9.101.234.169/testing_endpoint'
-        assert 'Basic' in responses_mock.calls[0].request.headers['Authorization']
-        assert response.status_code == 200
+    response = qpylib.REST('GET', 'testing_endpoint', verify='dummycert')
+    assert responses.calls[0].request.url == 'https://9.101.234.169/testing_endpoint'
+    assert 'Basic' in responses.calls[0].request.headers['Authorization']
+    assert response.status_code == 200
