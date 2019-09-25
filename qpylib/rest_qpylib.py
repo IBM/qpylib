080158a921b789cb737b63742e07837b28c694f4
michael.benson <mbenson@uk.ibm.com>
2019-09-25 09:55:11 +0100

    Add QRADAR_REST_PROXY for REST calls

--- qpylib/rest_qpylib.py
+++ qpylib/rest_qpylib.py
@@ -39,9 +39,10 @@ def _rest(rest_action, request_url, version, headers, data,
     rest_func = _choose_rest_function(rest_action)
     full_url = _generate_full_url(request_url)
     rest_headers = _add_headers(headers, version)
+    proxies = _add_proxies()
 
     return rest_func(full_url, headers=rest_headers, data=data, params=params,
-                     json=json_body, verify=verify, timeout=timeout)
+                     json=json_body, verify=verify, timeout=timeout, proxies=proxies)
 
 def _get_cert_filepath():
     with open('/proc/mounts') as mounts:
@@ -71,6 +72,12 @@ def _add_headers(headers, version=None):
         headers[SEC_HEADER] = sec_admin_token
 
     return headers
+
+def _add_proxies():
+    qradar_rest_proxy = os.getenv('QRADAR_REST_PROXY')
+    if qradar_rest_proxy is None:
+        return {}
+    return {'https': qradar_rest_proxy}
 
 def _generate_full_url(request_url):
     return "https://{0}/{1}".format(app_qpylib.get_console_fqdn(), request_url)
