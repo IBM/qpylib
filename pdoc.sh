e723159beb77b869f3b708f6b7315bac1e92153b
tristanlatr <tris.la.tr@gmail.com>
2020-09-21 13:49:57 -0400

    Made documentation publishable with the script pdoc.sh

--- pdoc.sh
+++ pdoc.sh
@@ -2,9 +2,32 @@
 # Copyright 2019 IBM Corporation All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
+set -e
 
 # Generating documentation programatically
 docs_folder="docs"
 python3 -m pdoc qpylib --output-dir ./${docs_folder} --html --force --template-dir ./.pdoc_templates
 
-# Then publish the documentation to github.io website for exemple
+echo "[SUCCESS] Documentation generated under ./${docs_folder}"
+
+# Then publish the documentation to github.io website 
+docs_user="tristanlatr" # To change
+docs_repo="qpylib-docs-testing" # To change
+
+github_site="https://github.com/${docs_user}/${docs_repo}.git"
+
+# Ask to publish
+read -p "[QUESTION] Do you want to publish the documentation? [y/n]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    rm -rf "./${docs_repo}"
+    git clone "${github_site}"
+    rm -rf "./${docs_repo}/*"
+    cp -R "./${docs_folder}/qpylib/" "./${docs_repo}/"
+    cd "${docs_repo}"
+    git add .
+    git commit -m "Generate ${keyword} docs $(date)" --quiet
+    git push origin master --quiet
+
+    echo "[SUCCESS] Documentation published: https://${docs_user}.github.io/${docs_repo}"
+fi
--- qpylib/__init__.py
+++ qpylib/__init__.py
@@ -1,6 +1,9 @@
 # Copyright 2019 IBM Corporation All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
+"""
+Welcome to the **qpylib** documentation. 
+"""
 
 __author__ = 'IBM'
 __version__ = 'unknown'
