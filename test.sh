79bace8e67d8af2d6e62e5cb9c4d4c0ecdb270f8
jthomperoo <j.thomperoo@hotmail.com>
2022-06-23 17:20:14 +0100

    Fix misreading CI params

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@
 # and not on the CI.
 GENERATE_HTML_REPORT=
 
-if [ "$CI" == "true" ]
+if [ -z "$CI" ]
 then
   GENERATE_HTML_REPORT="--cov-report html:coverage-html"
 fi
@@ -15,7 +15,7 @@ fi
 python -m pytest -v --cov-report xml:coverage.xml $GENERATE_HTML_REPORT --cov-report term --cov=qpylib test
 
 
-if [ "$CI" == "true" ]
+if [ -n "$CI" ]
 then
   # Workaround for SonarCloud not being able to read pytest coverage reports
   sed -i 's/\/home\/runner\/work\/qpylib\/qpylib\//\/github\/workspace\//g' coverage.xml
